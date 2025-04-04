a9543600cd5fe6bf9e754a271aef4d522bdd0b4c
ayu <umaminokiami@gmail.com>
2025-04-04 18:06:44 +0900

    fix: nitro.config

--- nitro.config.ts
+++ nitro.config.ts
@@ -5,6 +5,7 @@ export default defineNitroConfig({
   srcDir: 'server',
   compatibilityDate: '2025-04-01',
   modules: [nitroCloudflareDev],
+  preset: 'cloudflare-pages',
   experimental: {
     wasm: true,
   },
